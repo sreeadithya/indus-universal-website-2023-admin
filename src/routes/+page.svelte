a95ef342ac223e28e3185f48f9cf0c63869d4807
adithyasource <adithyaa@skiff.com>
2023-09-03 01:39:59 +0530

    Edit Announcements Works Perfectly + Changes to Auth

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,13 @@
   let userPassword;
 
   import { onAuthStateChanged } from "firebase/auth";
-  import { auth, logInButton, logOutButton } from "./firebase";
+  import { auth, logInButton } from "./firebase";
+
+  import { getNotificationsContext } from "svelte-notifications";
+  const { addNotification } = getNotificationsContext();
 
   let showLoggedIn = "none";
+  let logInError;
   let showLoggedOut = "none";
   let uid;
   onAuthStateChanged(auth, (user) => {
@@ -22,34 +26,50 @@
   });
 </script>
 
-<main class="col-span-10">
-  <div
-    class="flex flex-col justify-center items-center p-11 max-w-5xl"
-    style="display: {showLoggedOut}"
-  >
-    <img src="./logo.svg" alt="" class="w-[200px]" />
-    <form action="">
-      <input
-        type="email"
-        placeholder="email"
-        bind:value={userEmail}
-        name=""
-        class="py-3 pl-5 mb-2 rounded-lg border border-gray-300"
-      />
-      <input
-        type="password"
-        placeholder="password"
-        bind:value={userPassword}
-        name=""
-        class="py-3 mb-2 pl-5 rounded-lg border border-gray-300"
-      />
-      <button
-        on:click={logInButton(userEmail, userPassword)}
-        class="bg-neutral-950 text-white px-5 py-2 rounded-lg">Log In</button
-      >
-    </form>
+<div style="display: {showLoggedIn}" class="p-5 pl-10 pr-10 col-span-10">
+  select an option on the sidebar to begin
+</div>
+<div style="display: {showLoggedOut}">
+  <div class="w-screen h-screen flex flex-col items-center justify-center">
+    <div>
+      <img src="./logo.svg" alt="" class="h-[50px] mb-5" />
+      <form action="">
+        <input
+          type="email"
+          placeholder="email"
+          bind:value={userEmail}
+          name=""
+          class="py-3 px-5 rounded-lg border border-gray-300"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          bind:value={userPassword}
+          name=""
+          class="py-3 px-5 rounded-lg border border-gray-300"
+        /> <br />
+        <button
+          on:click={async () => {
+            if ((await logInButton(userEmail, userPassword)) != null) {
+              addNotification({
+                text: "Wrong email or password",
+                position: "bottom-center",
+                removeAfter: "5000",
+                type: "error",
+              });
+              userEmail = "";
+              userPassword = "";
+            }
+            // if (logInError == true) {
+
+            //   userEmail = "";
+            //   userPassword = "";
+            // }
+          }}
+          class="bg-neutral-950 text-white px-5 py-2 rounded-lg mt-5"
+          >Log In</button
+        >
+      </form>
+    </div>
   </div>
-  <div style="display: {showLoggedIn}">
-    select an option on the sidebar to begin
-  </div>
-</main>
+</div>
